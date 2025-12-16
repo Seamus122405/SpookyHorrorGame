d38b75f2e054e641c0f456ea2396d30356332ddf
Seamus122405 <seamus122405@gmail.com>
2025-12-16 16:11:46 -0500

    Dotween, PostProcessing, Flashlight
    Importing dotween, setting up test level, adding post processing, creating basic flashlight

---
+++ SpookyHorrorGame/Assets/Scripts/FlashlightScript.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class FlashlightScript : MonoBehaviour
+{
+
+    Light flashlightLight;
+
+    bool flashlightEnabled = false;
+    bool flashlightCanBeEnabled = true;
+
+    public float batteryPercent = 100f;
+    public float drainSpeed = 1f;
+
+    [SerializeField] Slider batterySlider;
+
+    Coroutine refillCoroutine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        flashlightLight = GetComponent<Light>();
+        
+        DisableFlashlight();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.fKey.wasPressedThisFrame && flashlightCanBeEnabled)
+        {
+            if(flashlightEnabled)
+            {
+                DisableFlashlight();
+            }
+            else
+            {
+                EnableFlashlight();
+            }
+        }
+
+        if(flashlightEnabled)
+        {
+            batteryPercent -= drainSpeed * Time.deltaTime;
+            batterySlider.value = batteryPercent;
+
+            if(batteryPercent <= 0f)
+            {
+                DisableFlashlight();
+            }
+        }
+    }
+
+    public void EnableFlashlight()
+    {
+        if (batteryPercent > 0)
+        {
+            flashlightEnabled = true;
+            flashlightLight.enabled = true;
+            StopCoroutine(refillCoroutine);
+        }
+
+    }
+
+    public void DisableFlashlight()
+    {
+        flashlightEnabled = false;
+        flashlightLight.enabled = false;
+
+        bool refillFromZero = !(batteryPercent > 0);
+        refillCoroutine = StartCoroutine(RefillFlashlight(refillFromZero));
+    }
+
+    IEnumerator RefillFlashlight(bool refillfromZero)
+    {
+        if (refillfromZero) { flashlightCanBeEnabled = false; }
+        yield return new WaitForSeconds(0.5f);
+
+        while(batteryPercent < 100f)
+        {
+            batteryPercent += drainSpeed * Time.deltaTime;
+            batterySlider.value = batteryPercent;
+            yield return null;
+        }
+
+        batteryPercent = 100f;
+        batterySlider.value = batteryPercent;
+        if (refillfromZero) { flashlightCanBeEnabled = true; }
+    }
+}
